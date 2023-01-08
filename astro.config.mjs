16415287268a36f7fe43b811c0def8cc2ab06538
josemasf <josemasf@gmail.com>
2023-01-08 14:00:48 +0100

    remove css

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from "astro/config";
 
 import tailwind from "@astrojs/tailwind";
+import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://josemariasantos.com/",
-  integrations: [tailwind(), robotsTxt()],
+  integrations: [
+    tailwind(),
+    robotsTxt(),
+    sitemap({
+      lastmod: new Date(),
+    }),
+  ],
 });
