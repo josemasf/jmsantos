0186b0e5e63352d0b545cabe90f8af5b82c6b5a0
josemasf <josemasf@gmail.com>
2023-01-08 20:54:10 +0100

    improve ux about

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,11 @@ import robotsTxt from "astro-robots-txt";
 // https://astro.build/config
 export default defineConfig({
   site: "https://josemariasantos.com/",
+  theme: {
+    container: {
+      center: true,
+    },
+  },
   integrations: [
     tailwind(),
     robotsTxt(),
