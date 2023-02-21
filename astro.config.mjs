7e7116327d0919c898f769381e2955d4c5cdf89c
Jose Maria Santos <innovation56@iberostar.com>
2023-02-21 09:58:47 +0100

    🚧 add cms

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
+import NetlifyCMS from "astro-netlify-cms";
 
 // https://astro.build/config
 import partytown from "@astrojs/partytown";
@@ -11,10 +12,36 @@ export default defineConfig({
   site: "https://josemariasantos.com/",
   theme: {
     container: {
-      center: true
-    }
+      center: true,
+    },
   },
-  integrations: [tailwind(), robotsTxt(), sitemap({
-    lastmod: new Date()
-  }), partytown()]
+  integrations: [
+    tailwind(),
+    robotsTxt(),
+    sitemap({
+      lastmod: new Date(),
+    }),
+    partytown(),
+    NetlifyCMS({
+      config: {
+        backend: {
+          name: "git-gateway",
+          branch: "main",
+        },
+        collections: [
+          {
+            name: "posts",
+            label: "Blog Posts",
+            folder: "src/pages/posts",
+            create: true,
+            delete: true,
+            fields: [
+              { name: "title", widget: "string", label: "Post Title" },
+              { name: "body", widget: "markdown", label: "Post Body" },
+            ],
+          },
+        ],
+      },
+    }),
+  ],
 });
