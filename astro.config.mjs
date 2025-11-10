7d23005fa0a074a5e7e4ecffd3bfae0ba4442f01
José María Santos Fernández (AIDA) <josantos+dalonso@domingoalonsogroup.com>
2025-11-10 11:50:50 +0100

    chore: add pnpm-workspace.yaml to specify only built dependencies

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,7 +2,6 @@ import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import sitemap from "@astrojs/sitemap";
 import robotsTxt from "astro-robots-txt";
-
 import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
@@ -10,10 +9,42 @@ export default defineConfig({
   site: "https://josemariasantos.com/",
   theme: {
     container: {
-      center: true
-    }
+      center: true,
+    },
   },
-  integrations: [tailwind(), robotsTxt(), sitemap({
-    lastmod: new Date()
-  }), partytown()]
+  build: {
+    inlineStylesheets: "auto",
+  },
+  compressHTML: true,
+  vite: {
+    build: {
+      cssCodeSplit: true,
+      rollupOptions: {
+        output: {
+          manualChunks: (id) => {
+            if (id.includes("node_modules")) {
+              if (id.includes("@cloudinary")) {
+                return "cloudinary";
+              }
+              return "vendor";
+            }
+          },
+        },
+      },
+    },
+  },
+  integrations: [
+    tailwind({
+      applyBaseStyles: false,
+    }),
+    robotsTxt(),
+    sitemap({
+      lastmod: new Date(),
+    }),
+    partytown({
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ],
 });
